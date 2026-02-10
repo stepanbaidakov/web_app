6d82dca85b72ea5bfac3e0fca03dcf7ccc92a402
Stepan Baidakov <stepanbaidakov@icloud.com>
2026-02-10 19:27:48 +0100

    homework 21.2 done

---
+++ main.py
@@ -0,0 +1,29 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        with open("contacts.html", "r", encoding="utf-8") as file:
+            page_content = file.read()
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
